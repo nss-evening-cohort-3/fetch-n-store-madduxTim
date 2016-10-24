75b676668c166f69119f29f39391f43f8994827a
Tim Maddux <timothymaddux@gmail.com>
2016-10-24 10:41:47 -0500

    Started getting results

--- Fetch-n-Store/Fetch-n-Store/Scripts/app.js
+++ Fetch-n-Store/Fetch-n-Store/Scripts/app.js
@@ -1,1 +1,14 @@
 ﻿var app = angular.module("FetchNStore", []);
+app.controller('callCtrl', function ($scope, $http) {
+    var url = "http://httpstat.us/201";
+    $scope.makeCall = () => {
+        $http.get(url)
+        .then(function (response) {
+            $scope.content = response.data;
+            $scope.statuscode = response.status;
+            $scope.statustext = response.statusText;
+            console.log(response);
+            //$scope.timeElapsed = response.blah;
+        });
+    }
+});
