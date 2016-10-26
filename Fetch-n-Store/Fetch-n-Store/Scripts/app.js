52729a26b7a765ada983446b1fa89c50fea3bfcf
Tim Maddux <timothymaddux@gmail.com>
2016-10-26 11:48:26 -0500

    Method select working

--- Fetch-n-Store/Fetch-n-Store/Scripts/app.js
+++ Fetch-n-Store/Fetch-n-Store/Scripts/app.js
@@ -1,18 +1,35 @@
 ﻿var app = angular.module("FetchNStore", []);
 app.controller("callCtrl", function ($scope, $http, $document) {
     var url = $document.find("#urlInput").val();
+    $scope.data = {
+        availableOptions: [
+        { id: "1", name: "---Pick one---" },
+        { id: "2", name: "GET" },
+        { id: "3", name: "HEAD" }
+        ],
+        selectedOption: { id: "1", name: "---Pick one---" }
+    };
+
     $scope.initiateRequest = () => {
         var startTime = Date.now();
-        var methodOfChoice = "GET";
         $http({
-            method: methodOfChoice,
+            //method: "GET",
+            //method: $scope.data.toString(),
+            method: $scope.data.selectedOption.name,
             url: url
         }).then(function successCallback(response) {
+            $scope.data.optDefault = "blah"; // unclear why I need this honestly... 
             console.log(response);
+            $scope.url = $document.find("#urlInput").val();
+            console.log($scope.data.selectedOption.name);
+            //$scope.methodUsed = $scope.data.toString();
+            //console.log($scope.data.toString());
+            //console.log(JSON.stringify($scope.data));
+            $scope.methodUsed = "GET";
             $scope.statusCode = response.status;
             $scope.statusText = response.statusText;
-            $scope.responseTime = Date.now() - startTime;
-            console.log(startTime);
+            $scope.responseTime = `${Date.now() - startTime} ms`;
+            //console.log(startTime);
         }, function errorCallback(response) {
             console.log(response);
         });
