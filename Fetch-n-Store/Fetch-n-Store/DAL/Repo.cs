8b8cf1ed5508ae4174d152ae3bb5bd8d32ba2e13
Tim Maddux <timothymaddux@gmail.com>
2016-10-31 12:27:44 -0500

    Finished setting up repo and tests.

---
+++ Fetch-n-Store/Fetch-n-Store/DAL/Repo.cs
@@ -0,0 +1,31 @@
+﻿using Fetch_n_Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fetch_n_Store.DAL
+{
+    public class Repo
+    {
+        public Context context { get; set; }
+        public Repo()
+        {
+            context = new Context();
+        }
+        public Repo(Context _context)
+        {
+            context = _context;
+        }
+        public List<Response> GetAllResponses()
+        {
+            return context.Responses.ToList();
+        }
+
+        public void AddNewResponse(Response response)
+        {
+            context.Responses.Add(response);
+            context.SaveChanges();
+        }
+    }
+}
